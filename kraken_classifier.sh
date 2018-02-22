e618ddf3a7ebb9c5a3eaa9fce67703426e83c266
Alfred Ssekagiri <assekagiri@gmail.com>
2018-02-22 15:57:30 +0300

    Delete kraken_classifier.sh

--- kraken_classifier.sh
+++
@@ -1,5 +0,0 @@
-while read p;
-	do 
-		mkdir -p /path/to/${p}_kraken
-		kraken --preload --threads 24 --fastq-input --paired --db /path/to/krakendb ${p}_L001_R1.fq ${p}_L001_R2.fq > /path/to/${p}_kraken ;
-done < /path/to/prefix.txt ;
